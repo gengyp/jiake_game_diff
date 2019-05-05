88db68c38efb7a5326301fb83be2f1fcb00db397
gengyp <gengyanpeng12@gmail.com>
2019-05-05 14:38:35 +0800

    增加 gitignore

--- c5game.py
+++ c5game.py
@@ -45,13 +45,12 @@ def get_data(ip_lst):
   headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
   # dota2
   url = "https://www.igxe.cn/dota2/570"
-  appid = eval(url.split('/')[-1])
   for i in range(circles[0]):
     proxy = {'http': 'http://' + random.choice(ip_lst)}
     querystring = {"is_buying":"0","is_stattrak[]":["0","0"],"price_to":"1","price_to":"1000","sort":"2","ctg_id":"0","type_id":"0","page_no":"{}".format(i+1),
       "page_size":"20","rarity_id":"0","exterior_id":"0","quality_id":"0","capsule_id":"0","_t":"1557025475913"} # 出售
     r = requests.request("GET", url, headers=headers, proxies=proxy, params=querystring)
-    save_data2db(appid,r.text)
+    save_data2db(r.text)
     time.sleep(0.5)
 
   for i in range(circles[1]):
@@ -59,18 +58,17 @@ def get_data(ip_lst):
     querystring = {"is_buying":"1","is_stattrak[]":["0","0"],"price_to":"1","price_to":"1000","sort":"2","ctg_id":"0","type_id":"0","page_no":"{}".format(i+1),
       "page_size":"20","rarity_id":"0","exterior_id":"0","quality_id":"0","capsule_id":"0","_t":"1557026884738"} # 求购
     r = requests.request("GET", url, headers=headers, proxies=proxy, params=querystring)
-    save_data2db(appid,r.text)
+    save_data2db(r.text)
     time.sleep(0.5)
 
   # H1Z1
   url = "https://www.igxe.cn/h1z1/433850"
-  appid = eval(url.split('/')[-1])
   for i in range(circles[2]): # 21
     proxy = {'http': 'http://' + random.choice(ip_lst)}
     querystring = {"is_buying":"0","is_stattrak[]":["0","0"],"price_to":"1","price_to":"1000","sort":"2","ctg_id":"0","type_id":"0","page_no":"{}".format(i+1),
       "page_size":"20","rarity_id":"0","exterior_id":"0","quality_id":"0","capsule_id":"0","_t":"1557034298679"} # 出售
     r = requests.request("GET", url, headers=headers, proxies=proxy, params=querystring)
-    save_data2db(appid,r.text)
+    save_data2db(r.text)
     time.sleep(0.5)
 
   for i in range(circles[3]):
@@ -78,11 +76,13 @@ def get_data(ip_lst):
     querystring = {"is_buying":"1","is_stattrak[]":["0","0"],"price_to":"1","price_to":"1000","sort":"2","ctg_id":"0","type_id":"0","page_no":"{}".format(i+1),
       "page_size":"20","rarity_id":"0","exterior_id":"0","quality_id":"0","capsule_id":"0","_t":"1557026884738"} # 求购
     r = requests.request("GET", url, headers=headers, proxies=proxy, params=querystring)
-    save_data2db(appid,r.text)
+    save_data2db(r.text)
     time.sleep(0.5)
 
-def save_data2db(appid,html):
+def save_data2db(html):
   tree = etree.HTML(html)
+  total_pages = tree.xpath('//*[@id="page-content"]/a[3]/@page_no')[0]
+  print('current page total {}'.format(total_pages),end='\r')
 
   goods_names = tree.xpath('//div[@class="dataList"]/a/div[2]/@title')
   amounts = tree.xpath('//div[@class="dataList"]/a/div[3]/div/span/text()')
@@ -94,7 +94,7 @@ def save_data2db(appid,html):
     goods_name = goods_names[i].replace("'",'')
     good_status = good_statuses[i].split('：')[0]
     good_num = eval(good_statuses[i].split('：')[1])
-    lst.append([appid,goods_name,amounts[i]+amount_subs[i],good_status,good_num])
+    lst.append([570,goods_name,amounts[i]+amount_subs[i],good_status,good_num])
 
   # store valid proxies into db.
   print ("\n>>>>>>>>>>>>>>>>>>>> Insert to database Start  <<<<<<<<<<<<<<<<<<<<<<")
